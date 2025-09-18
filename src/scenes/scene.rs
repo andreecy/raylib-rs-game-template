339da05d415dc96b1437821a1c1b4e87b32458b1
andreecy <andreechristianyulianto@gmail.com>
2025-09-18 11:17:13 +0700

    fix transition

--- src/scenes/scene.rs
+++ src/scenes/scene.rs
@@ -18,6 +18,7 @@ pub trait Scene {
 
 pub struct SceneManager {
     current_scene: Box<dyn Scene>,
+    next_scene: Option<Box<dyn Scene>>,
     on_transition: bool,
     trans_fadeout: bool,
     trans_alpha: f32,
@@ -27,6 +28,7 @@ impl SceneManager {
     pub fn new() -> Self {
         Self {
             current_scene: Box::new(MainMenuScene::new()),
+            next_scene: None,
             on_transition: false,
             trans_fadeout: false,
             trans_alpha: 0.0,
@@ -35,18 +37,27 @@ impl SceneManager {
 
     fn start_transition(&mut self) {
         self.on_transition = true;
-        self.trans_fadeout = false;
+        self.trans_fadeout = true;
         self.trans_alpha = 0.0;
     }
 
     fn update_transition(&mut self) {
-        if !self.trans_fadeout {
+        if self.trans_fadeout {
+            // fade out
             self.trans_alpha += 0.05;
             if self.trans_alpha > 1.01 {
                 self.trans_alpha = 1.0;
-                self.trans_fadeout = true;
+                self.trans_fadeout = false; // switch to fade in
+
+                // perform the change scene now that screen is black
+                if let Some(next_scene) = self.next_scene.take() {
+                    self.current_scene.destroy();
+                    self.current_scene = next_scene;
+                    self.current_scene.init();
+                }
             }
         } else {
+            // fade in
             self.trans_alpha -= 0.05;
             if self.trans_alpha < -0.01 {
                 self.trans_alpha = 0.0;
@@ -59,10 +70,9 @@ impl SceneManager {
         if !self.on_transition {
             let change_request = self.current_scene.update(d);
             if let SceneChange::Change(new_scene) = change_request {
+                // Store the new scene and start the fade-out
+                self.next_scene = Some(new_scene);
                 self.start_transition();
-                self.current_scene.destroy();
-                self.current_scene = new_scene;
-                self.current_scene.init();
             }
         } else {
             self.update_transition();
